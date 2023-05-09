0e24160d081e7004b305d7c5d7a18c7191266556
htmldiana <dianekhomenko@gmail.com>
2023-05-09 20:24:11 +0200

    11

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 export const API_KEY = `0a0fad420f8b287917c1cf2e9edfaf94`;
 
-export const getMovieById = id => {
-  const response = axios
-    .get(`movie/${id}?api_key=${API_KEY}`)
-    .then(function (response) {
-      console.log(response.data);
-    });
-
+export const getMovieById = async id => {
+  const response = await axios.get(`movie/${id}?api_key=${API_KEY}`);
   return response.data;
 };
