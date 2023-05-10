efcc9ab9b467d01e1faa23d32cbb36e3ad43d196
htmldiana <dianekhomenko@gmail.com>
2023-05-10 21:26:16 +0200

    main back

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { MovieListItem } from 'components/MovieListItem/MovieListItem';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 
 axios.defaults.baseURL = `https://api.themoviedb.org/3/`;
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
+    const location = useLocation();
+
 
   useEffect(() => {
     async function getData() {
@@ -20,7 +24,11 @@ export const MovieList = () => {
   return (
     <ul>
       {movies.map(movie => (
-        <Link to={`/movies/${movie.id}`} key={movie.id}>
+        <Link
+          to={`/movies/${movie.id}`}
+          key={movie.id}
+          state={{ from: location }}
+        >
           <MovieListItem title={movie.title} />
         </Link>
       ))}
